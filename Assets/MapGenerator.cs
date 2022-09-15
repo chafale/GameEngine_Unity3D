f0f7e1ecd4d741ba8c0aad8da49a8b5746e72ed8
sanya12 <sanya.shaikh82@gmail.com>
2022-09-14 21:45:47 -0700

    Added Alphabets objects

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -15,8 +15,16 @@ public class MapGenerator : MonoBehaviour {
 	public GameObject obstacle2;
 	public GameObject obstacle3;
 	public GameObject obstacle4;
+
+	public GameObject character1;
+	public GameObject character2;
 	
 	public GameObject obstaclePrefab;
+	public GameObject A;
+	public GameObject C;
+	public GameObject E;
+
+	public List<GameObject> displayCharacter = new List<GameObject>();
 	
 	public float minObstacleY;
 	public float maxObstacleY;
@@ -29,10 +37,16 @@ public class MapGenerator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		displayCharacter.Add(A);
+	    displayCharacter.Add(C);
+	    displayCharacter.Add(E);
 		obstacle1 = GenerateObstacle(player.transform.position.x + 10);
 		obstacle2 = GenerateObstacle(obstacle1.transform.position.x);
 		obstacle3 = GenerateObstacle(obstacle2.transform.position.x);
 		obstacle4 = GenerateObstacle(obstacle3.transform.position.x);
+		character1 = GenerateCharacter(obstacle1.transform.position.x);
+		character2 = GenerateCharacter(obstacle4.transform.position.x);
+
 	}
 	
 	GameObject GenerateObstacle(float referenceX) {
@@ -40,10 +54,21 @@ public class MapGenerator : MonoBehaviour {
 		SetTransform(obstacle,referenceX);
 		return obstacle;
 	}
+
+	GameObject GenerateCharacter(float referenceX) {
+		int num = Random.Range(0,3);
+		GameObject obstacle = GameObject.Instantiate(displayCharacter[num]);
+		SetTransformCharacter(obstacle,referenceX);
+		return obstacle;
+	}
 	
 	void SetTransform(GameObject obstacle, float referenceX) {
 		obstacle.transform.position = new Vector3(referenceX + Random.Range(minObstacleSpacing, maxObstacleSpacing), Random.Range(minObstacleY, maxObstacleY), 0);
 		obstacle.transform.localScale = new Vector3(obstacle.transform.localScale.x, Random.Range(minObstacleScaleY, maxObstacleY), obstacle.transform.localScale.z);
+	}
+
+	void SetTransformCharacter(GameObject obstacle, float referenceX) {
+		obstacle.transform.position = new Vector3(referenceX + Random.Range(minObstacleSpacing, maxObstacleSpacing), Random.Range(minObstacleY, maxObstacleY), 0);
 	}
 	
 	// Update is called once per frame
@@ -64,10 +89,12 @@ public class MapGenerator : MonoBehaviour {
 			obstacle1 = obstacle2;
 			obstacle2 = obstacle3;
 			obstacle3 = obstacle4;
-			
 			SetTransform(tempObstacle, obstacle3.transform.position.x);
 			obstacle4 = tempObstacle;
-			
+			if (player.transform.position.x > character2.transform.position.x){
+				character1 = GenerateCharacter(obstacle2.transform.position.x);
+		    	character2 = GenerateCharacter(obstacle4.transform.position.x);
+			}
 		}
 	}
 }
