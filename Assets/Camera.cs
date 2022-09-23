4b92ebf2491d8ed50f04204f20aab7cf10567209
Vaishnav Addagulla <addagull@usc.edu>
2022-09-23 00:13:36 -0700

    Decrease life count when wrong letter is collected and restart on 0 lives

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -5,42 +5,42 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Camera : MonoBehaviour {
-	
-	public Player player;
-	public GameObject Background;
-	public static GameObject overReset;
-	
-	// Use this for initialization
-	void Start () {
-		overReset = GameObject.Find("Background");
-		overReset.SetActive(false);
-	}
-		
-	
-	// Update is called once per frame
-	void Update () {
-		if (!player.gameOver) 
-		{
-			transform.position += new Vector3(5f * Time.deltaTime, 0, 0);
-			
-		}
-		else	
-		{
-			
-			GameEnd();
-			
-		}
-	}
-	 
-	public static void GameEnd(){
-		// GameObject.Find("Background").SetActive(true);
-		overReset.SetActive(true);
-	}
-	public void Reset()
-	{
-		
-		Debug.Log("Reset");
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-	}
+    
+    public Player player;
+    public GameObject Background;
+    public static GameObject overReset;
+    
+    // Use this for initialization
+    void Start () {
+        overReset = GameObject.Find("Background");
+        overReset.SetActive(false);
+    }
+        
+    
+    // Update is called once per frame
+    void Update () {
+        if (!player.gameOver) 
+        {
+            transform.position += new Vector3(5f * Time.deltaTime, 0, 0);
+            
+        }
+        else    
+        {
+            
+            GameEnd();
+            
+        }
+    }
+     
+    public static void GameEnd(){
+        // GameObject.Find("Background").SetActive(true);
+        LivesScript.lives = 3;
+        overReset.SetActive(true);
+    }
+    public void Reset()
+    {
+        
+        Debug.Log("Reset");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
-
--- Assets/Destroy.cs
+++ Assets/Destroy.cs
@@ -6,18 +6,30 @@ using mg = GameManager;
 public class Destroy : MonoBehaviour
 {
     void OnCollisionEnter(Collision collide) {
-		//Debug.Log("objected collided");
+        //Debug.Log("objected collided");
         string collided_letter = gameObject.tag;
         //Debug.Log("Collided with letter : " + collided_letter);
         Destroy(gameObject);
         char inputLetter = char.Parse(collided_letter);
-        
+        int c = 0;
         for (int i = 0; i < mg.solvedList.Count; i++)
         {   
             Debug.Log(mg.solvedList[i]);
             if(mg.solvedList[i] == inputLetter){
                 mg.letterHolderList[i].text = inputLetter.ToString();
+                c=1;
+                break;
             }
         }
-	}
+        if(c == 0){
+            Debug.Log("You hit the wrong letter");
+            if(LivesScript.lives > 0) {
+                LivesScript.lives -= 1;
+            }
+            if(LivesScript.lives == 0){
+                Camera.GameEnd();
+                Player.body.isKinematic = true;
+            }
+        }
+    }
 }
