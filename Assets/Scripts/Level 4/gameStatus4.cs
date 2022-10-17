03b4329c05a23199dab508e3e168ed518f30d470
Ayush Kothari <aayushkothari11@yahoo.com>
2022-10-16 21:19:02 -0700

    Levels

---
+++ Assets/Scripts/Level 4/gameStatus4.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+using des = Destroy4;
+using mg = GameManager4;
+
+
+public class gameStatus4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] TextMeshProUGUI statusText;
+    public Player player;
+
+    public static gameStatus4 gameStatusObj;
+    public static int score = MapGenerator4.displayCharacter.Count;
+
+    void Awake()
+    {
+        gameStatusObj = this;
+    }
+    void Start()
+    {
+        statusText = GetComponent<TMPro.TextMeshProUGUI>();
+        player = gameObject.AddComponent<Player>();
+    }
+
+    // Update is called once per frame
+    public void updateStatus()
+    {   //when health is zero
+        if (FindObjectOfType<Player>().currentHealth <= 0 )
+        {
+            Debug.Log("Game end health: "+ FindObjectOfType<Player>().currentHealth);
+            statusText.text = "Game Over! :( \n\n The correct word was : " + mg.correct_word + "\nScore : " + ScoringSystem.myScore;
+            PlayerPrefs.SetInt("gameStatus", 1);
+            Camera4.GameEnd();
+        }
+        else
+        { //when player wins
+            statusText.text = "Congratulations! You Win!\n\n The correct word was: " + mg.correct_word + "\nScore : " + ScoringSystem.myScore;
+            PlayerPrefs.SetInt("gameStatus", 1);
+            //Camera.GameEnd();
+        }
+    }
+
+}
