b37781e91ca6fbc77d50e0eb626a3d130246ec7d
Ashwin Chafale <chafale@usc.edu>
2022-11-28 20:52:52 -0800

    Level 2.2

---
+++ Assets/Scripts/Level 2.1/gameStatus2_1.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+using des = Destroy2_1;
+using mg = GameManager2_1;
+
+
+public class gameStatus2_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] TextMeshProUGUI statusText;
+    public Player player;
+
+    public static gameStatus2_1 gameStatusObj;
+    public static int score = MapGenerator2_1.displayCharacter.Count;
+
+    void Awake()
+    {
+        gameStatusObj = this;
+    }
+    void Start()
+    {
+        statusText = GetComponent<TMPro.TextMeshProUGUI>();
+        player = gameObject.AddComponent<Player>();
+    }
+
+    // Update is called once per frame
+    public void updateStatus()
+    {   //when health is zero
+
+        if (FindObjectOfType<Player>().currentHealth <= 0 )
+        {
+            mg.gamag.LoseAudioPlayer.Play();
+            Debug.Log("Game end health: "+ FindObjectOfType<Player>().currentHealth);
+            statusText.text = "Game Over! :( \n\n The correct word was : " + mg.correct_word + "\nScore : " + ScoringSystem.myScore;
+
+            // Analytics : winStatus, score
+            PlayerPrefs.SetInt("winStatus", 0);
+            PlayerPrefs.SetInt("score", ScoringSystem.myScore);
+
+            // Camera2.GameEnd();
+        }
+        else
+        { //when player wins
+            mg.gamag.WinAudioPlayer.Play();
+            statusText.text = "Congratulations! You Win!\n\n Score : " + ScoringSystem.myScore;
+
+            // Analytics : winStatus, score
+            PlayerPrefs.SetInt("winStatus", 1);
+            PlayerPrefs.SetInt("score", ScoringSystem.myScore);
+
+            //Camera.GameEnd();
+        }
+    }
+
+}
