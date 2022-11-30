239d43d49dfad89a7e49d6cac930e5070e37918a
Ashwin Chafale <chafale@usc.edu>
2022-11-29 18:50:41 -0800

    Level 2.3

---
+++ Assets/Scripts/Level 2.2/Camera2_2.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using mg = GameManager2_2;
+using mapgen = MapGenerator2_2;
+using TMPro;
+using pl = Player;
+
+public class Camera2_2 : MonoBehaviour {
+
+    public Player player;
+    public GameObject Background;
+    public static GameObject overReset;
+
+    public GameObject Hintground;
+    public static GameObject goldReset;
+
+    public static GameObject completeLevelUI;
+    // Use this for initialization
+    public AudioSource GameoverBGM;
+
+    public AudioSource GamestartBGM;
+
+    public static Camera2_2 camObj;
+
+    void Awake()
+    {
+        camObj = this;
+    }
+
+    void Start () {
+
+        goldScript2_2.goldIndex = 0;
+        overReset = GameObject.Find("Background");
+        overReset.SetActive(false);
+        // goldReset = GameObject.Find("Hintground");
+        // goldReset.SetActive(false);
+        Time.timeScale = 1;
+        GamestartBGM.Play();
+    }
+    // Update is called once per frame
+    void Update () {
+        if (!player.gameOver)
+        {
+            transform.position += new Vector3(5.5f * Time.deltaTime, 0, 0);
+
+        }
+        else
+        {
+
+            if(FindObjectOfType<Player>().currentHealth <= 0){
+              gameStatus2_2.gameStatusObj.updateStatus();
+            }
+            Camera2_2.GameEnd();
+            //GameEnd();
+            player.currentHealth = 0;
+            FindObjectOfType<HealthBar>().SetHealth(player.currentHealth);
+            Debug.Log("I am obstacle here");
+        }
+        //if (player.currentHealth == 0)
+        // if (player.currentHealth <= 0 )
+        // {
+        //     //gameStatus.gameStatusObj.updateStatus();
+        //     GameEnd();
+        //     Debug.Log("I am health here");
+        // }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Debug.Log("Space key was pressed.");
+            pl.playerSpeed+=3;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            // Debug.Log("Space key was released.");
+            pl.playerSpeed-=3;
+        }
+
+
+    }
+
+     public void updateBGM()
+    {
+        GamestartBGM.Stop();
+    }
+    public static void GameEnd(){
+
+        GameObject[] scoreA;
+        GameObject[] healthA;
+        scoreA = GameObject.FindGameObjectsWithTag("ScoreAnim");
+        healthA = GameObject.FindGameObjectsWithTag("HealthAnim");
+
+        foreach (GameObject scoreAnim in scoreA)
+        {
+            scoreAnim.SetActive(false);
+        }
+        foreach (GameObject healthAnim in healthA)
+        {
+            healthAnim.SetActive(false);
+        }
+        // GameObject.Find("Background").SetActive(true);
+        mg.solvedList = new List<char>();
+        mg.letterHolderList = new List<TMP_Text>();
+        mg.chars = new List<GameObject>();
+        mapgen.displayCharacter = new List<GameObject>();
+        mapgen.correctCharacters = new List<GameObject>();
+        GameManager2_2 gameMananger = GameObject.Find("GameManager").GetComponent<GameManager2_2>();
+        Time.timeScale = 0;
+        // mapgen.displayCharacter = new List<GameObject>();
+        goldScript2_2.goldText.text="";
+        overReset.SetActive(true);
+        gameMananger.HealPopup.SetActive(false);
+        ScoringSystem.myScore = 0;
+        mapgen.healCharacters = new List<GameObject>();
+        mg.healHolderList = new List<TMP_Text>();
+        Camera2_2.camObj.updateBGM();
+    }
+
+    public void Reset()
+    {
+        Debug.Log("Reset");
+        mg.solvedList = new List<char>();
+        mg.letterHolderList = new List<TMP_Text>();
+        mg.healHolderList = new List<TMP_Text>();
+        mapgen.correctCharacters = new List<GameObject>();
+        mapgen.healCharacters = new List<GameObject>();
+        mapgen.displayCharacter = new List<GameObject>();
+        goldScript2_2.goldText.text="";
+        ScoringSystem.myScore = 0;
+        mapgen.healCharacters = new List<GameObject>();
+        mg.healHolderList = new List<TMP_Text>();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public static void Pause()
+    {
+        Time.timeScale = 0;
+        goldReset.SetActive(true);
+    }
+    public static void Resume()
+    {
+        Time.timeScale = 1;
+        goldReset.SetActive(false);
+    }
+    public static void CompleteLevel()
+    {
+
+        completeLevelUI.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Level 2.2/goldScript2_2.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class goldScript2_2 : MonoBehaviour
+{
+    public static int goldIndex;
+    public static int currGoldIndex;
+
+    public static string [][] hint_array = new string[][] {
+                                                new string[] {"I am tall when I am young, and I am short when I am old. What am I?","I am the light of your darkness","You blow me on a special day","I melt on heat"},
+                                                new string[] {"What is always in front of you but can not be seen?","You are sometimes worried about me","I am unpredictable","Only Dr.Strange can see me"},
+                                                new string[] {"What word is pronounced the same if you take away four of its five letters?","You are in this when you call customer care","Can be broken if you know someone in this","You often stand in this"},
+                                                new string[] {"I have many keys but can’t open a single door","You have to learn to be able to use me","You can have your career built around me","I sound on touch"},
+                                                new string[] {"They come at night without being called ","We are lost in the day without being stolen","You see me daily","You are ONE ;)"},
+                                                new string[] {"What has hands but cannot clap","The entity insude me changes every second","I point without fingers","Without arms I strike, Without feet I run"},
+                                                new string[] {"I can fly but have no wings. What am I?", "I can cry but I have no eyes", "Wherever I go, darkness follows me","I sometimes play with thunder" },
+                                                new string[] {"The man who makes it doesn’t use it","The man who buys it doesn’t need it","The man who uses it doesn’t know it.","The cave I sleep in fits me well.Where am I?"},
+                                                new string[] {"I don't have eyes, but once I did see","Once I had thoughts, but now I'm white and empty.","I hold the smartest organ of the body","I am often used on Halloween"},
+                                                new string[] {"It can be cracked","It can be made","It can be told","It can be played","I am usually funny"} };
+ 
+    [SerializeField] public static TextMeshProUGUI goldText;
+
+    public static string[] goldList;
+
+    public static void assign_values(){
+        goldList = hint_array[GameManager2_2.index];
+        // Debug.Log(goldIndex);
+        // Debug.Log(goldList);
+    }
+
+    public static goldScript2_2 goldObj;
+    void Awake()
+    {
+        goldObj = this;
+    }
+    void Start()
+    {
+        goldText = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    IEnumerator SetCountText()
+    {
+        Debug.Log("hi inside");
+        yield return new WaitForSeconds(2);
+        goldText.text = "";
+    }
+
+
+    // Update is called once per frame
+    public void updateHint(int temp)
+    {
+        if(goldIndex > 3)
+        {
+            goldText.text = "";
+        }
+        else
+        {
+            if(temp==100)
+            {
+                goldText.text = "New Hint: " + goldList[goldIndex].ToString();
+            }
+            else if(temp==101){
+                goldText.text = "Speed Power up Collected !!";
+            }
+            else if(temp==102){
+                goldText.text = "Health Power up Collected !!";
+            }
+            else{
+                goldText.text = "Autofill Power up Collected !!";
+            }
+            StartCoroutine(SetCountText());
+        }
+    }
+}
